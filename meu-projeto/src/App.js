41106071455cdb4990e274d791c3faeb95f604f5
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-11-14 17:03:08 -0300

    aula 02

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Olá react</h1>
+     <p>meu primeiro projeto</p>
+    </div>
+  );
+}
+
+export default App;
