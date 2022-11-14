6c41d86b298dd1347d0fc02b045743e45e8dbf56
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-11-14 17:33:47 -0300

    aula 03

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var name = 'Gabriel'
+
+  var newname = name.toUpperCase()
+  
+  function sum (a,b){
+    return a + b
+  }
+
+  var url = 'https://via.placeholder.com/150'
+  
   return (
     <div className="App">
      <h1>Olá react</h1>
      <p>meu primeiro projeto</p>
+     <p>olá, {newname}</p>
+     <p>soma: {sum(1,5)}</p>
+     <img src={url} alt='minha imagem'/>
+     
     </div>
   );
 }
