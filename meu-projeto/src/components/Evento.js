9a0b2718ebb5ca2191a7ce53e43639fe77bdadaa
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-11-16 22:03:46 -0300

    aula11

--- meu-projeto/src/components/Evento.js
+++ meu-projeto/src/components/Evento.js
@@ -9,6 +9,9 @@ function Evento({numero }){
     return(
         <>
         <p>Clicque para disparar um evento:</p>
+        
+        {/*evento por props*/}
+        <Button text='Primeiro evento'/>
         <button onClick={meuEvento}>Ativar</button>
         </>
     )
