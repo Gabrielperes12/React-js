adf7fc48bd88c5998cb02428ca6c73e2a8e98f09
Gabriel Peres <114517457+Gabrielperes12@users.noreply.github.com>
2022-11-20 03:06:49 -0300

    aula 16

--- meu-projeto/src/Components/Layout/Fotter.js
+++ meu-projeto/src/Components/Layout/Fotter.js
@@ -1,4 +1,16 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Fotter.module.css'
+
 function Fotter(){
-    return <footer>Rodapé</footer>
+    return (
+    <footer className='{style.social_list}'>
+        <ul >
+            <li> <FaFacebook/></li>
+            <li> <FaInstagram/></li>
+            <li> <FaLinkedin/></li>
+           
+        </ul>
+        <p>Nosso rodapé</p>
+    </footer>)
 }
 export default Fotter
